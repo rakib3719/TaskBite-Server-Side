725387f59ddcc8a6fad737d78d46b3e94347da8d
rakib3719 <bannah76769@gmail.com>
2024-06-01 13:27:23 +0600

    user data save to db all functionality done

--- index.js
+++ index.js
@@ -41,8 +41,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+
+const userCollection = client.db('taskBite').collection('users')
 
     app.post('/jwt', async (req, res) => {
         const user = req.body;
@@ -56,24 +59,49 @@ async function run() {
 
     const verifyToken = (req, res, next)=>{
 
-console.log('inside verify token', req.headers);
+console.log(req.headers.authorization);
 
-if(!req.headers.authoraization){
+if(!req?.headers?.authorization){
 
 return res.status(401).send({message: 'forbidden access'})
 
 }
 
 
-const token = req.headers.authoraization.split(' ')[1];
+const token = req.headers.authorization.split(' ')[1];
+
 
 jwt.verify(token, process.env.TOKEN_SECRET,(err, decoded)=>{
 
+if(err){
+    return res.status(401).send({message: 'forbidden access'})
+} 
 
-    
+req.decoded = decoded;
+console.log("ata decoded", req.decoded);
+
+next()
+
 })
 
     }
+
+
+
+ app.post('/userAdd',async (req,res)=>{
+
+const userInfo = req.body;
+const query ={email: userInfo?.email}
+const isExist = await userCollection.findOne(query);
+if(isExist){
+    return res.send(isExist)
+}
+
+console.log(userInfo);
+const result =await userCollection.insertOne(userInfo)
+res.send(result)
+
+ })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
